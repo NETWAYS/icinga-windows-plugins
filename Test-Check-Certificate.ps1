209bc5616d8acc59a3c7af5cf53d6353b5ca1e30
Markus Frosch <markus.frosch@netways.de>
2020-03-18 17:17:23 +0100

    Update Check-Certificate

--- Test-Check-Certificate.ps1
+++ Test-Check-Certificate.ps1
@@ -1,6 +1,6 @@
 # Testing the Check-Certificate plugin
 
-# Set-Location $PSScriptRoot
+Set-Location $PSScriptRoot
 
 Write-Host 'Icinga 2 local certificates'
 $rc = .\Check-Certificate.ps1 -CertPath C:\ProgramData\icinga2\var\lib\icinga2\certs -CertName *.crt -CriticalStart 2019-03-01
@@ -9,5 +9,10 @@ Write-Host
 
 Write-Host 'Local cert stores'
 $rc = .\Check-Certificate.ps1 -CertStore '*' -CertSubject '*Microsoft*'
+Write-Host "Status: ${rc}"
+Write-Host
+
+Write-Host 'Testing for errors'
+.\Check-Certificate.ps1 -CertPath C:\ProgramData\icinga2\var\lib\icinga2\certs -CertName ca.crt,'*.key','*.xxx' -CriticalStart 2019-03-01
 Write-Host "Status: ${rc}"
 Write-Host
